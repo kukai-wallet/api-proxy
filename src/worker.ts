d0d0b273be970cfa16286077e596dca785436b9c
Alex <gounisalex@gmail.com>
2023-07-11 23:04:03 +0200

    restricts content (only json is allowed)

--- src/worker.ts
+++ src/worker.ts
@@ -3,7 +3,13 @@ const HEADERS = {
 	'Cache-Control': 'max-age: 10'
 }
 
-const MISSING_URI_ERROR = 'Bad request: Missing uri'
+const CONTENT_TYPE = 'Content-Type'
+const APPLICATION_JSON = 'application/json'
+
+enum ERRORS {
+	MISSING_URI_ERROR = 'Bad request: Missing uri',
+	NOT_JSON = 'Error: content-type is not json'
+}
 
 export default {
 	async fetch(request: Request, _env: Env, _ctx: ExecutionContext): Promise<Response> {
@@ -12,10 +18,14 @@ export default {
 		const proxyUri = url.searchParams.get('uri')
 
 		if (!proxyUri) {
-			return new Response(MISSING_URI_ERROR, { status: 400 })
+			return new Response(ERRORS.MISSING_URI_ERROR, { status: 400 })
 		}
 
 		const response = await fetch(decodeURIComponent(proxyUri), request)
+
+		if (!response.headers.get(CONTENT_TYPE)?.includes(APPLICATION_JSON)) {
+			return new Response(ERRORS.NOT_JSON, { status: 400 })
+		}
 
 		return new Response(response.body, { headers: HEADERS })
 	},
